8115aa3a2095b9a84d171b734469d0630a5c48b4
AlexGlem <87341686+AlexGlem@users.noreply.github.com>
2021-07-20 18:47:09 +0300

    laba 15
    добавлена функция с куками 
    возможно добавлять другие сайты

--- yandex_bot_alfAV.js
+++ yandex_bot_alfAV.js
@@ -6,20 +6,27 @@
 // @author       Alfimova AV
 // @match        https://yandex.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://crushdrummers.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let yandexInput = document.getElementById("text");
 let btnYaGo = document.getElementsByClassName("button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited")[0];
-let words = ["как звучит гобой","флейта","кларнет","валторна","саксофон"];
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai": ["как звучит гобой","флейта","кларнет","валторна","саксофон"],
+    "crushdrummers.ru": ["барабанное шоу","Шоу барабанщиков Crush","заказать барабанное шоу"]
+}
+let site = Object.keys(sites)[getIntRandom(0, Object.keys(sites).length)];
+let words = sites[site];
+//let words = ["как звучит гобой","флейта","кларнет","валторна","саксофон"];
 let word = words[getIntRandom(0, words.length)];
-
 
 if(btnYaGo != undefined){
     let i=0;
     let timerId = setInterval(function(){
         yandexInput.value = yandexInput.value + word[i++]; // пишем по буквам фразу в ПС
+        document.cookie = "site="+site;
         if(i==word.length){
             clearInterval(timerId);
             btnYaGo.click();
@@ -28,9 +35,10 @@ if(btnYaGo != undefined){
 }else if(location.hostname === "yandex.ru"){
     let links = document.links;
     let goNext = true;
+    let site = getCookie("site");
     for(let i=0; i<links.length; i++){
         let link = links[i];
-        if(link.href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+        if(link.href.indexOf(site) != -1){
             /*link.target = "_self";*/
             setTimeout(function(){
                 link.click();
@@ -40,24 +48,38 @@ if(btnYaGo != undefined){
         }
     }
     if(goNext){
-        let nextYa = document.getElementsByClassName("link link_theme_none link_target_serp pager__item pager__item_kind_next")[0];
-        setTimeout(function(){
-            nextYa.click();
-        }, 3000);
+        let currentPage = document.getElementsByClassName('pager_item pageritem_current_yes pager_item_kind_page')[0];
+        if(currentPage<10){
+            let nextYa = document.getElementsByClassName("link link_theme_none link_target_serp pager__item pager__item_kind_next")[0];
+            setTimeout(function(){
+                nextYa.click();
+            }, 3000);
+        }else{
+            location.href = "https://yandex.ru/";
+        }
     }
 }else{
-    let links = document.links;
-    let randomIndex = getIntRandom(0, links.length);
-    let link = links[randomIndex];
-    if(link.href.indexOf(location.hostname) != -1){
-        setTimeout(function(){
+    setInterval(function(){
+        if(getIntRandom(0,100)<30) location.href = "https://yandex.ru/";
+        let links = document.links;
+        let randomIndex = getIntRandom(0, links.length);
+        let link = links[randomIndex];
+        if(link.href.indexOf(location.hostname) != -1){
             links[randomIndex].click();
-        }, 2000);
-    }else{
-        location.href = "https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/";
-    }
+        }else{
+            location.href = location.origin;
+        }
+    }, 2000);
 }
 
 function getIntRandom(min, max){
-    return Math.floor(Math.random()*(max-min)+min);
+        return Math.floor(Math.random()*(max-min)+min);
 }
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
